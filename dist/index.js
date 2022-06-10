87ee4e0c9572d564756c4938ccff2eacc6ac3784
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-10 20:58:33 +0200

    Adds basic cli.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
 "use strict";
-console.log("wassup");
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+commander_1.program.version("1.0.0").description("Clean Next.js Boilerplate CLI");
+commander_1.program.parse();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("wassup");
+import { program } from "commander";
+
+program.version("1.0.0").description("Clean Next.js Boilerplate CLI");
+
+program.parse();
