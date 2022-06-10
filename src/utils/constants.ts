28184fc77233144aed088125545e5622bca8ce27
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-10 22:14:07 +0200

    Adds constants.

---
+++ src/utils/constants.ts
@@ -0,0 +1,102 @@
+export const VERCEL_JSON = `
+{
+    "github": {
+        "silent": true
+    }
+}
+`;
+
+export const GITIGNORE = `
+# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# next.js
+/.next/
+/out/
+
+# production
+/build
+
+# misc
+.DS_Store
+*.pem
+
+# debug
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+# local env files
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+# vercel
+.vercel
+`;
+
+export const UTILS_INDEX = `
+export const BASE_URL =
+	process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+`;
+
+export const API_POST = `
+const handler = (req, res) => {
+	res.status(200).json({ message: "Hello there" });
+};
+
+export default handler;
+`;
+
+export const APP_TSX = `
+import { useEffect } from "react";
+
+import Wrapper from "../components/Wrapper";
+
+const MyApp = ({ Component, pageProps }) => {
+	useEffect(() => {
+		const disableReactDevTools = () => {
+			const noop = () => undefined;
+			const DEV_TOOLS = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+			if (typeof DEV_TOOLS === "object") {
+				for (const [key, value] of Object.entries(DEV_TOOLS)) {
+					DEV_TOOLS[key] = typeof value === "function" ? noop : null;
+				}
+			}
+		};
+		if (process.env.NODE_ENV === "production") disableReactDevTools();
+	}, []);
+
+	return (
+		<Wrapper>
+			<Component {...pageProps} />
+		</Wrapper>
+	);
+};
+
+export default MyApp;
+`;
+
+export const WRAPPER = `
+import Meta from "./Meta";
+
+const Wrapper = ({ children }) => {
+	return (
+		<>
+			<Meta />
+			{children}
+		</>
+	);
+};
+
+export default Wrapper;
+`;
