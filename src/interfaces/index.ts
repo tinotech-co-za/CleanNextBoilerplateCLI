7f75a82815f8561a9b674fcd284f52832eab2c1c
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-12 16:01:05 +0200

    Adds jsdoc.

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,6 @@
+/**
+ * The options for a permutation. Including whether or not the project uses TypeScript as well as whether or not to include Sass.
+ */
 export type Options = {
 	typeScript: boolean;
 	sass: boolean;
