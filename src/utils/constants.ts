9e5aeab6dea59d3cd0c11a54e3c9f95c9826b87d
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-28 16:59:33 +0200

    Adds elipsise function.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -55,6 +55,15 @@ yarn-error.log*
  */
 export const UTILS_INDEX = `export const BASE_URL =
 	process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+
+	/**
+ * Appends a limit to the end of text and substrings to the specified limit.
+ * @param text The text to add an ellips to.
+ * @param limit The point at which the text is cut off.
+ * @returns A string cut off at the limit and with an ellips appended to the end.
+ */
+export const elipsise = (text: string, limit: number): string =>
+	text.substring(0, limit - 7)+"...";
 `;
 
 /**
