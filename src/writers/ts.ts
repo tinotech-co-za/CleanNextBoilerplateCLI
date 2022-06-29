13bba94938a8aa8fb88980a6ae46e19a34f751fc
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-29 09:49:19 +0200

    Adds tsconfig writer.

--- src/writers/ts.ts
+++ src/writers/ts.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 
-import { INTERFACES } from "../utils/constants";
+import { INTERFACES, TSCONFIG } from "../utils/constants";
 
 /**
  * Uses fs to write the interfaces file to the directory specified.
@@ -9,4 +9,12 @@ import { INTERFACES } from "../utils/constants";
  */
 export const writeInterfaces = async (basePath: string): Promise<void> => {
 	fs.writeFileSync(path.join(basePath, "/interfaces/index.ts"), INTERFACES);
+};
+
+/**
+ * Uses fs to write a TSConfig file to the directory specified.
+ * @param basePath The base path to write out the files to.
+ */
+export const writeTSConfig = async (basePath: string): Promise<void> => {
+	fs.writeFileSync(path.join(basePath, "tsconfig.json"), TSCONFIG);
 };
