37bf1b96d6f049a2da8cfe5c760adad40a4ac2cf
Tino Muzambi <tinomuzambi@gmail.com>
2022-07-05 10:39:18 +0200

    Adds write reducer writer.

--- src/writers/context.ts
+++ src/writers/context.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import fs from "fs";
+import { APP_REDUCER, APP_REDUCER_TS } from "../utils/constants";
 
 /**
  * Uses fs to write out all the directories necessary for using Context in a Next.js app.
@@ -9,6 +10,21 @@ const makeContextDirs = async (basePath: string): Promise<void> => {
 	fs.mkdirSync(path.join(basePath, "/context"), {
 		recursive: true,
 	});
+};
+
+/**
+ * Uses fs to write out the reducer file for using the Context API.
+ * @param basePath The base path to write out the files to.
+ * @param typeScript Whether this project uses TypeScript or not.
+ */
+const writeReducer = async (
+	basePath: string,
+	typeScript: boolean
+): Promise<void> => {
+	fs.writeFileSync(
+		path.join(basePath, `/context/AppReducer.${typeScript ? "ts" : "tsx"}`),
+		typeScript ? APP_REDUCER_TS : APP_REDUCER
+	);
 };
 
 /**
