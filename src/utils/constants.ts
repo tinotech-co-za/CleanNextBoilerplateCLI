a4a4eafd0a36005e191c82ea55444c281ee4c637
Tino Muzambi <tinomuzambi@gmail.com>
2022-07-05 10:01:17 +0200

    Adds app context.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -539,3 +539,21 @@ export const APP_REDUCER = `const Reducer = (state, action) => {
 	switch (action.type) {
 	}
 };`;
+
+/**
+ * Context provider for including JavaScript Context API.
+ * context/AppContext.jsx
+ */
+export const APP_CONTEXT = `import { createContext, useReducer } from "react";
+
+import AppReducer from "./AppReducer";
+
+const initialState = {};
+
+export const AppContext = createContext(initialState);
+
+export const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	return <AppContext.Provider>{children}</AppContext.Provider>;
+};`;
