db0889e9eeaed14517abbac5dfbb78a6581f17be
Tino Muzambi <tinomuzambi@gmail.com>
2022-07-06 13:41:34 +0200

    Adds js sass eslint context and mongo writer.

--- src/writers/permutations.ts
+++ src/writers/permutations.ts
@@ -520,6 +520,35 @@ export const writeJavaScriptSassESLintContext = async (
 };
 
 /**
+ * Generates boilerplate for a Next.js project that uses JavaScript, Sass, ESLint, Context and MongoDB.
+ * @param name The name of the project.
+ * @param basePath The base path to write out the files to.
+ * @param options The options. Takes the form. {
+	typeScript: boolean;
+	sass: boolean;
+	context: boolean;
+} where typeScript is if this is a TypeScript object, context is whether the Context API is included and where sass is whether this project should include sass.
+ * @returns void.
+ */
+export const writeJavaScriptSassESLintMongoDBContext = async (
+	name: string,
+	basePath: string,
+	options: Options
+): Promise<void> => {
+	await writeCommonFiles(name, basePath, options);
+	await writeJSSassESLintMongoPackageJson(name, basePath);
+	await writeMongo(basePath, false);
+	await writeESLint(basePath);
+	await writeSass(basePath);
+	await writeContext(basePath, options.typeScript);
+	console.log(
+		chalk.green(
+			"\n- Generated Next.js app with JavaScript, Sass, ESLint, Context and MongoDB"
+		)
+	);
+};
+
+/**
  * Generates boilerplate for a Next.js project that uses JavaScript, Sass, ESLint and MongoDB.
  * @param name The name of the project.
  * @param basePath The base path to write out the files to.
